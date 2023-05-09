d07e6c2fc5a1f478b89eededf060960e73ac17ed
Denister <denister@mailbox.unideb.hu>
2023-05-09 16:03:24 +0200

    Bookrepository osztályok hozzáadása

---
+++ Konyvtar.WEBAPI/BookRepository.cs
@@ -0,0 +1,27 @@
+﻿namespace Konyvtar.WEBAPI
+{
+    public class BookRepository : IBookRepository
+    {
+
+        private readonly Dictionary<int, Book> _people = new();
+        public void Delete(int id)
+        {
+            _people.Remove(id);
+        }
+
+        public Book Get(int id)
+        {
+           return _people.TryGetValue(id, out var book) ? book : null;
+        }
+
+        public IEnumerable<Book> GetAll()
+        {
+            return _people.Values;
+        }
+
+        public void Upsert(Book book)
+        {
+            _people[book.Id] = book;
+        }
+    }
+}
---
+++ Konyvtar.WEBAPI/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Konyvtar.WEBAPI
+{
+    public interface IBookRepository
+    {
+
+        void Upsert (Book book);
+
+        void Delete(int id);
+        IEnumerable <Book> GetAll();
+
+        Book Get(int id);
+    }
+}
